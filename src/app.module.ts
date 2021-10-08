60ca282851365450f98fdf4570bb0c1ae810eb13
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-07 22:09:30 -0300

    feat(category): generate resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { PrismaService } from './prisma/prisma.service';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService, PrismaService],
-  imports: [ProductModule],
+  imports: [ProductModule, CategoryModule],
 })
 export class AppModule {}
